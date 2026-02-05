c55048016be7170ec016168cd42b8c78dd5ded36
Cond <volteire77@gmail.com>
2026-02-05 19:11:11 +0100

    fixed pigpio

--- initial_logic.py
+++ initial_logic.py
@@ -3,32 +3,35 @@
 from config import Config
 config = Config()
 
-import RPi.GPIO as GPIO
+import pigpio
 from threading import Thread
 import time
 
-MOTOR_1 = 18
-MOTOR_2 = None
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_1, GPIO.OUT)
-
+    
 class Move():
     def __init__(self):
+        self.pi = pigpio.pi()
+        if not self.pi.connected:
+            exit()
+        self.MOTOR_1 = 18
+        self.MOTOR_2 = None
+        self.pi.set_mode(self.MOTOR_1, pigpio.OUTPUT)
         self.delay =  config.delay
         self.run = config.run
+        self.pi.set_PWM_frequency(self.MOTOR_1,1000)
         Thread(target=self.update, daemon=False).start()
         
     def update(self):
-        self.moveFwd()
+        while self.run == True:
+            self.moveFwd()
+        self.stop()
     
     def moveFwd(self):
-        pwm = GPIO.PWM(MOTOR_1, 500)
-        if self.run == True:
-            pwm.start(0)
-            pwm.ChangeDutyCycle(50)
-            time.sleep(self.delay)
-            self.run = False
-        else:
-            pwm.stop()
-            GPIO.cleanup()
+        self.pi.set_PWM_dutycycle(self.MOTOR_1,128)
+        time.sleep(config.delay)
+        self.run = False
+
+    def stop(self):
+        self.pi.set_PWM_dutycycle(self.MOTOR_1,0)
+        self.pi.stop()
